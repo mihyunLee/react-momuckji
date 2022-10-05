c320e24c2ad549ff28faa463a86f449ae1070175
mihyunLee <mihy621@gmail.com>
2022-10-05 16:52:45 +0900

    style: App.js GlobalStyle 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+   @font-face {
+      font-family: 'BMHANNAPro';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_seven@1.0/BMHANNAPro.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+  }
+  *{
+    font-family: "BMHANNAPro";
+  }
+  body{
+    background: #fff;
+  }
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
