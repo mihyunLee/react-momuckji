5a6509c2cd6505f10ee16f2d9637adb4290a06ac
mihyunLee <mihy621@gmail.com>
2022-10-05 23:40:01 +0900

    feat: Home 버튼 이벤트 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,20 @@ import React from "react";
 import Button from "../components/Button";
 import HomeTemplate from "../components/HomeTemplate";
 import Slider from "../components/Slider";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <HomeTemplate>
       <Slider direction="left" />
       <Slider direction="right" />
-      <Button text={"메뉴 찾기"} search={true} />
+      <Button
+        text={"메뉴 찾기"}
+        search={true}
+        onClick={() => navigate("/category")}
+      />
     </HomeTemplate>
   );
 };
